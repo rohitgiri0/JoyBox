71ff0801b2325d07c8eda77de2751ece0a39dc23
rohitgiri0 <rajendragiri364@gmail.com>
2025-08-04 02:41:21 +0530

    good night

--- JoyBox/settings.py
+++ JoyBox/settings.py
@@ -112,7 +112,7 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-POSTGRES_LOCALLY=False
+POSTGRES_LOCALLY=True
 if ENVIRONMENT=='production' or POSTGRES_LOCALLY==True:
     DATABASES['default']=dj_database_url.parse(env('DATABASE_URL'))
 
